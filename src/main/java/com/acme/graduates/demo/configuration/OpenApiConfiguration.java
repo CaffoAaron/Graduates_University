056307e2149a4884ec6ec612fb0d548646d0730c
Aaron Alberto Alva Caffo <34250041+CaffoAaron@users.noreply.github.com>
2021-01-10 13:13:04 -0500

    documentacion completed

--- src/main/java/com/acme/graduates/demo/configuration/OpenApiConfiguration.java
+++ src/main/java/com/acme/graduates/demo/configuration/OpenApiConfiguration.java
@@ -1,4 +1,22 @@
 package com.acme.graduates.demo.configuration;
 
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class OpenApiConfiguration {
+    @Bean(name = "graduatesOpenApi")
+    public OpenAPI graduatesOpenApi() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(new Info()
+                        .title("Graduates Application API")
+                        .description(
+                                "Graduates API implemented with Spring Boot RESTful service and documented using springdoc-openapi and OpenAPI 3.0"));
+
+    }
+
 }
