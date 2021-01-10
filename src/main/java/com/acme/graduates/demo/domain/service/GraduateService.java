e576631893e94704a7fd233ea14285ae872727d1
Aaron Alberto Alva Caffo <34250041+CaffoAaron@users.noreply.github.com>
2021-01-10 11:49:39 -0500

    service completed

--- src/main/java/com/acme/graduates/demo/domain/service/GraduateService.java
+++ src/main/java/com/acme/graduates/demo/domain/service/GraduateService.java
@@ -1,4 +1,19 @@
 package com.acme.graduates.demo.domain.service;
 
+
+import com.acme.graduates.demo.domain.model.Graduate;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+
 public interface GraduateService {
+    Page<Graduate> getAllGraduates(Pageable pageable);
+
+    Graduate getGraduatesById(Long graduatesId);
+
+    Graduate createGraduate(Graduate graduate);
+
+    Graduate updateGraduate(Long graduateId, Graduate graduate);
+
+    ResponseEntity<?> deleteGraduate(Long graduateId);
 }
--- src/main/java/com/acme/graduates/demo/domain/service/UserService.java
+++ src/main/java/com/acme/graduates/demo/domain/service/UserService.java
@@ -1,5 +1,18 @@
 package com.acme.graduates.demo.domain.service;
 
+import com.acme.graduates.demo.domain.model.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 
 public interface UserService {
+    Page<User> getAllUser(Pageable pageable);
+
+    User getUserById (Long userId);
+
+    User createUser (User user);
+
+    User updateUser (Long userId, User user);
+
+    ResponseEntity<?> deleteUser(Long userId);
 }
